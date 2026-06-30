073531073297ce943217a580d9e06e6c360b0b67
Nitin Saroj <nitinsaroj1732005@gmail.com>
2026-06-30 15:48:56 +0530

    Time: 3 ms (46.29%), Space: 9.8 MB (49.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        unordered_map<char,int> map1;
-        unordered_map<char,int> map2;
+        unordered_map<char,int> map;
         for(int i=0;i<s.length();i++){
-            map1[s[i]]++;
+            map[s[i]]++;
         }
-        for(int i=0;i<t.length();i++){
-            map2[t[i]]++;
-        }
-        for(auto x:map1){
-            char ch1=x.first;
-            int freq1=x.second;
-            if(map2.find(ch1)!=map2.end()){
-                int freq2= map2[ch1];
-                if(freq1!=freq2) return false;
+        for(int i=0;i<s.length();i++){
+            char ch=t[i];
+            if(map.find(ch)!=map.end()){
+                map[ch]--;
+                if(map[ch]==0) map.erase(ch);
             }
             else return false;
         }
+        if(map.size()>0) return false;
         return true;
     }
 };
