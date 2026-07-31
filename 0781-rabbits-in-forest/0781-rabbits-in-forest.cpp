e4984f1b8039cba8ebcda728d16896b5b8bf42f5
Nitin Saroj <nitinsaroj1732005@gmail.com>
2026-07-31 16:14:46 +0530

    Time: 0 ms (100%), Space: 12.1 MB (67.21%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& ans) {
+        unordered_map<int,int> mp;
+        int result=0;
+        for(int i=0;i<ans.size();i++){
+            if(!mp[ans[i]+1]){
+                result+=ans[i]+1;
+                if(ans[i]==0) continue;
+                mp[ans[i]+1]=1;
+            }
+            else{
+                mp[ans[i]+1]++;
+                int key=ans[i]+1;
+                int val=mp[key];
+                if(key==val){
+                    mp.erase(key);
+                }
+            }
+        }
+        return result;
+    }
+};
